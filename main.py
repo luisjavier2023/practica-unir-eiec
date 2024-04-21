e1997d76e6c6ae46e405d30dfd47beacfbc1d92d
Sebasmmn <smayorga@ec.krugercorp.com>
2024-04-20 22:11:15 -0500

    Se agregó nueva funcionalidad de ejecución para ordenar asc o desc y se modificó el archivo readme

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sys
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
+DEFAULT_ASC_DESC = True # Usar True para ascendente y False para descendente
 
 
 def sort_list(items, ascending=True):
@@ -15,6 +16,12 @@ def sort_list(items, ascending=True):
         raise RuntimeError(f"No puede ordenar {type(items)}")
 
     return sorted(items, reverse=(not ascending))
+
+def descending_sort_list(items, ascending=False):
+    if not isinstance(items, list):
+        raise RuntimeError(f"No puede ordenar {type(items)}")
+
+    return sorted(items, reverse=(not ascending))   
 
 
 def remove_duplicates_from_list(items):
@@ -24,12 +31,15 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    asc_desc_value = DEFAULT_ASC_DESC
+    if len(sys.argv) == 4:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
+        asc_desc_value = sys.argv[3].lower() == "yes"
     else:
         print("Se debe indicar el fichero como primer argumento")
         print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("El tercer argumento indica si el orden es ascendente o ingles")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
@@ -46,4 +56,7 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    if asc_desc_value:
+        print(sort_list(word_list))
+    else:
+        print(descending_sort_list(word_list))
